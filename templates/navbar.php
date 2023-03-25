4b5ad72fe6edd2e3c2cc5dfaba33553dbacfa043
color.branch <philippe.malezieux@afnor.org>
2023-03-25 02:22:39 +0100

    ajout navbar

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,18 +1,18 @@
-<nav class="bg-gray-800">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 bg-gr">
-        <div class="relative flex items-center justify-between h-16">
-            <div class="flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div class="flex-shrink-0-right pl-25">
+<nav class="bg-gray-800 pb-">
+    <div class="justify-between max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 bg-gr">
+        <div class="justify-between items-center h-16">
+            <div class="flex flex-nowrap justify-between sm:justify-start">
+                <div class="flex flex-nowrap justify-between ">
                     <a href="{{ path('app_user') }}" class="text-white font-bold text-xl ">
-                       <img class="mr-20" src="{{asset('gefor.png')}}">
+                       <img class="mr-20" src="{{asset('geforbis.png')}}">
                     </a>
                 </div>
-                <div class="flex-shrink-5-right pl-25 p-25">
+                <div class="flex flex-nowrap justify-between">
                     <a href="{{ path('app_user') }}" class="text-white font-bold text-xl ">
                        MyWebsite
                     </a>
                 </div>
-                <div class="hidden sm:block sm:ml-6 pl-50">
+                <div class="flex flex-nowrap justify-between sm:block sm:ml-6 pl-50">
                     <div class="flex space-x-4">
                         <a href="{{ path('app_user') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Users</a>
                         <a href="{{ path('app_user') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
